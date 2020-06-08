91c24fc6a552fee624124d20703145a9f1d48b5f
nicolas.gonzalez93@outlook.com <nicolas.gonzalez93@outlook.com>
2020-06-07 20:51:16 -0400

    new version

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
-const body = document.getElementsByTagName('body')[0];
-const tablaTamano = 5;
-const posFinal = tablaTamano.toString() + tablaTamano.toString();
+tablaTamano = 5;
+posFinal = this.tablaTamano.toString() + this.tablaTamano.toString();
+pasos = 0;
 this.initTabla();
 
 function initTabla() {
+  const body = document.getElementsByTagName('body')[0];
   const tabla = document.createElement('table');
-  for (let x = 1; x <= tablaTamano; x++) {
+  for (let x = 1; x <= this.tablaTamano; x++) {
     const fila = document.createElement('tr');
-    for (let y = 1; y <= tablaTamano; y++) {
+    for (let y = 1; y <= this.tablaTamano; y++) {
       const celda = document.createElement('td');
       celda.id = x.toString() + y.toString();
-      if ((x === tablaTamano && y === tablaTamano) || (x === 1 && y === 1)) {
+      if ((x === this.tablaTamano && y === this.tablaTamano) || (x === 1 && y === 1)) {
         celda.classList.add('color');
+      }else{
+        celda.addEventListener('click', () => clickCelda(celda.id));
       }
-      celda.addEventListener('click', () => clickCelda(celda.id));
       fila.appendChild(celda);
     }
     tabla.appendChild(fila);
@@ -29,6 +31,7 @@ function clickCelda(id) {
       alert('Celda ya marcada.');
     } else {
       celda.classList.add('color');
+      this.pasos++;
     }
   } else {
     alert('Movimiento incorrecto');
@@ -39,33 +42,46 @@ function verifyCeldas(id) {
   const ejex = id[0];
   const ejey = id[1];
   const casos = [
-    (parseInt(ejex) + 1) + '' + ejey, // x+1 y
-    (parseInt(ejex) - 1) + '' + ejey, // x-1 y
-    ejex + '' + (parseInt(ejey) + 1), // x y+1
-    ejex + '' + (parseInt(ejey) - 1), // x y-1
-    (parseInt(ejex) - 1) + '' + (parseInt(ejey) - 1), // x-1 y-1
-    (parseInt(ejex) + 1) + '' + (parseInt(ejey) - 1), // x+1 y-1
-    (parseInt(ejex) - 1) + '' + (parseInt(ejey) + 1), // x+1 y-1
-    (parseInt(ejex) + 1) + '' + (parseInt(ejey) + 1), // x+1 y+1
+    (parseInt(ejex) + 1) + '' + ejey, // x+1 y (Abajo)
+    (parseInt(ejex) - 1) + '' + ejey, // x-1 y (Arriba)
+    ejex + '' + (parseInt(ejey) + 1), // x y+1 (Derecha)
+    ejex + '' + (parseInt(ejey) - 1), // x y-1 (Izquierda)
+    // (parseInt(ejex) - 1) + '' + (parseInt(ejey) - 1), // x-1 y-1
+    // (parseInt(ejex) + 1) + '' + (parseInt(ejey) - 1), // x+1 y-1
+    // (parseInt(ejex) - 1) + '' + (parseInt(ejey) + 1), // x+1 y-1
+    // (parseInt(ejex) + 1) + '' + (parseInt(ejey) + 1), // x+1 y+1
   ];
   let val = false;
-  casos.forEach(pos => {
-    if(pos === posFinal){
-      setTimeout(() => {
-        alert('Juego finalizado.');
-      }, 100);
-    }
-    if (verifyCelda(pos)) {
+  let casosPintados = [];
+  casos.forEach(caso => {
+    if (verifyCelda(caso) ) {
+      casosPintados.push(caso);
       val = true;
     }
   });
+
+  casosPintados.forEach(caso => {
+    if(caso === casos[2]){
+      val = false;
+    }
+  });
+
+  if(val){
+    casos.forEach(caso => {
+      if(caso === this.posFinal){
+        setTimeout(() => {
+          alert('Juego finalizado. Pasos realizados: ' + this.pasos);
+        }, 100);
+      }
+    });
+  }
   return val;
 }
 
 function verifyCelda(id) {
   const celda = document.getElementById(id);
   if (celda) {
-    if (celda.classList.contains('color')) {
+    if (celda.classList.contains('color') && celda.id !== this.posFinal) {
       return true;
     } else {
       return false;
